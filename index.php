23227d0341c826afbdc6acc8f5fa9c501c364f6a
sadat <sadat.himel@gmail.com>
2021-01-18 00:13:20 +0600

    upload image successfully

--- index.php
+++ index.php
@@ -17,15 +17,22 @@ $db =  new Database();
                         $unique_image = substr(md5(time()), 0, 10).'.'.$file_ext;
                         $uploaded_image = "uploads/".$unique_image;
 
-                        move_uploaded_file($file_temp, $uploaded_image);
-                        $query = "INSERT INTO tbl_image(image) VALUES ('$uploaded_image')";
-                        $inserted_rows = $db->insert($query);
-                        if ($inserted_rows) {
-                            echo "<span class='success'>Image Inserted Successfully</span>";
-                        }else {
-                            echo "<span class='error'>Image Not Inserted !</span>";
+                        if (empty($file_name)) {
+                            echo "<span class='error'>Please select any Image</span>";
+                        } elseif($file_size > 1048567) {
+                            echo "<span class='error'>Image Size should be less then 1MB</span>";
+                        } elseif(in_array($file_ext, $permited) === false) {
+                            echo "<span class='error'>You can upload only:-".implode(',',$permited)."</span>";
+                        } else {
+                            move_uploaded_file($file_temp, $uploaded_image);
+                            $query = "INSERT INTO tbl_image(image) VALUES ('$uploaded_image')";
+                            $inserted_rows = $db->insert($query);
+                            if ($inserted_rows) {
+                                echo "<span class='success'>Image Inserted Successfully</span>";
+                            } else {
+                                echo "<span class='error'>Image Not Inserted !</span>";
+                            }
                         }
-                        
                     }
                 ?>
                 <form action="" method="post" enctype="multipart/form-data">
@@ -41,6 +48,15 @@ $db =  new Database();
                     </table>
                 </form>
 
+                <?php
+                    $query = "SELECT * FROM tbl_image order by id desc limit 1";
+                    $getImage = $db->select($query);
+                    if ($getImage) {
+                        while ($result = $getImage->fetch_assoc()) {
+                ?>
+                <img src="<?php echo $result['image']; ?>" alt="" height="100px" width="100px">
+                <?php } } ?>
+            </div>
 <?php
 include "inc/footer.php";
 ?>
