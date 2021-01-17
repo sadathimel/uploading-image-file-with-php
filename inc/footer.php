c3504a57c46770a140c7aac8b93ff1efbdfbc1f6
sadat <sadat.himel@gmail.com>
2021-01-17 22:50:17 +0600

    create insert file in database

---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+
+</div>
+</section>
+<section class="footeroption">
+    <h2>This is a footer.</h2>
+</section>
+</div>
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Uploading Image File with PHP</title>
+    <style>
+        body{font-family: Verdana}
+        .phpcoding{width: 900px;margin: 0 auto; background: $ddd;}
+        .headeroption,.footeroption{background: #444; color: #fff;text-align: center;padding: 20px;}
+        .headeroption h2,.footeroption h2{margin: 0;}
+        .mainoption{min-height: 420px; padding: 20px;}
+        .myform{width: 500px;border: 1px solid #999; margin: 0 auto; padding: 0 20px 20px;}
+        .success{color: green; font-size: 18px; margin-top: 10px;}
+        .error{color: red; font-size: 18px; margin-top: 10px;}
+        input[type="submit"],input[type="file"]{cursor: pointer;}
+    </style>
+</head>
+<body>
+<div class="phpcoding">
+    <section class="headeroption">
+        <h2>Uploading Image File with PHP</h2>
+    </section>
+    <section class="mainoption">
